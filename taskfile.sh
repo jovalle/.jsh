296f355a92f92a593345d03150026281f7c5a92b
Jay Ovalle <jay.ovalle@gmail.com>
2025-02-19 20:06:50 -0500

    Better OS handling

--- taskfile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ ! -x /usr/local/bin/task ]; then
-  sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b .
-  sudo install -o root -g root -m 0755 task /usr/local/bin/task
-fi
-[ -f ./task ] && rm -f ./task
