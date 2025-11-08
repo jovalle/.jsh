41b0c0bd1e8bfb6bff4644c6fa0992d490d0d21a
Jay Ovalle <jay.ovalle@gmail.com>
2025-11-08 12:51:04 -0500

    fix(eslint): lint error

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+// ESLint v9+ configuration
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        // Browser globals
+        window: 'readonly',
+        document: 'readonly',
+        console: 'readonly',
+        // Node.js globals
+        process: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly',
+      },
+    },
+    rules: {
+      // Google Style Guide recommendations
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'prefer-arrow-callback': 'warn',
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-trailing-spaces': 'error',
+      'eol-last': ['error', 'always'],
+      indent: ['error', 2],
+      quotes: ['error', 'single', { avoidEscape: true }],
+      semi: ['error', 'always'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off', // Allow console for scripts
+    },
+  },
+];
